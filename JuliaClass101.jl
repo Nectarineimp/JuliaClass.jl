ee295681409b81c2a1d390b92d0a73ad10820e9c
Peter Mancini <pete@nectarineimp.com>
2017-04-27 00:12:10 -0500

    First file added to Julia 101 class repository.

---
+++ JuliaClass101.jl
@@ -0,0 +1,13 @@
+# JuliaClass101.jl
+
+# Julia is a low ceremony language like Python
+# but built for concurrency like Clojure
+# and fast like C++, but with multiple dispatch
+
+# Julia is built on top of the LLVM which powers
+# languages like Swift (Apple).
+
+# Basic tour of types
+# Julia is type optional. The base type is
+
+println("Hello, World.")
